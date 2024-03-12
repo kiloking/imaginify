b5e2e1131cdbaf8d3463ab9129c394de97f5882f
wood.wu <kilokingw@gmail.com>
2024-03-12 20:43:29 +0800

    UPdated

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -10,7 +10,7 @@ import { handleError } from "../utils";
 export async function createUser(user: CreateUserParams) {
   try {
     await connectToDatabase();
-
+    console.log('createUser')
     const newUser = await User.create(user);
 
     return JSON.parse(JSON.stringify(newUser));
